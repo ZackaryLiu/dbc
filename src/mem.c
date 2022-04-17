1c810b9c879b97f1a143ab9f6d9066c11cc6af0f
zackary liu <zackary.liu.pro@gmail.com>
2022-04-17 14:06:56 +0800

    mem: preliminarily add sysinfo
    Signed-off-by: zackary liu <zackary.liu.pro@gmail.com>

--- src/mem.c
+++ src/mem.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <init.h>
+#include <sys/sysinfo.h>
 
+static int print_sys_info(void)
+{
+	struct sysinfo info = { 0 };
+	int ret;
+
+	if ((ret = sysinfo(&info)) != 0) {
+		printf("sysinfo err.\n");
+		return ret;
+	}
+
+	
+}
 
 static int mem_init(void)
 {
