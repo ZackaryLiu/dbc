02fe58084b02dd625a6a33339f666a3bcd545a6f
zackary liu <zackary.liu.pro@gmail.com>
2022-04-23 23:03:18 +0800

    mem: get some basic mem info
    Signed-off-by: zackary liu <zackary.liu.pro@gmail.com>

--- src/mem.c
+++ src/mem.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <init.h>
 #include <sys/sysinfo.h>
+#include <unistd.h>
 
 static int print_sys_info(void)
 {
@@ -12,12 +13,35 @@ static int print_sys_info(void)
 		return ret;
 	}
 
-	
+	puts("__________________________________");
+	printf("uptime: %ld\n", info.uptime);
+
+	puts("__________________________________");
+	printf("loads: %ld %ld %ld\n", info.loads[0], info.loads[1], info.loads[2]);
+
+	puts("__________________________________");
+	printf("total: %ld\tfree: %ld\tshm: %ld\tbuffer: %ld\n", 
+		info.totalram, info.freeram, info.sharedram, info.bufferram);
+	printf("totalswap: %ld\tfreeswap: %ld\n", info.totalswap, info.freeswap);
+	printf("mem_unit: %d\n", info.mem_unit);
+
+	/* TODO: How does sysconf work */
+	printf("pagesize: %ld\n", sysconf(_SC_PAGESIZE));
+
+	puts("__________________________________");
+	printf("total procs: %d\n", info.procs);
+
+	puts("__________________________________");
+	printf("totalhigh: %ld\tfreehigh: %ld\n", info.totalhigh, info.freehigh);
+
+	return 0;
 }
 
 static int mem_init(void)
 {
 	puts("mem_init ok.");
+
+	print_sys_info();
 
 	return 0;
 }
